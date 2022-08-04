abad811e70392c65cc047370405587c8396ccda3
sylwanbouderbala <sylwanbouderbala@outlook.fr>
2022-08-04 16:55:51 +0200

    ajout string

---
+++ main.c
@@ -0,0 +1,14 @@
+//
+// Created by sylwan bouderbala on 04/08/2022.
+//
+
+#include "main.h"
+int main(int argc, char **argv)
+{
+    int i = 0;
+    while(i < argc)
+    {
+        ft_putstr(argv[i]);
+        i ++;
+    }
+}
---
+++ main.h
@@ -0,0 +1,15 @@
+//
+// Created by sylwan bouderbala on 04/08/2022.
+//
+
+#ifndef LIBFT_MAIN_H
+#define LIBFT_MAIN_H
+#include <stdlib.h>
+#include <libft.h>
+typedef enum {
+    product = '*',
+    power = '^',
+    add = '+',
+    divide = '/',
+}operations;
+#endif //LIBFT_MAIN_H
