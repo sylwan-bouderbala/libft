22d59d1df90d47cc6d8dd8f783c674bf08f57b70
sylwan-bouderbala <52298274+sylwan-bouderbala@users.noreply.github.com>
2022-08-04 18:02:17 +0200

    ajout ft_strcat mais il faut toujours gerer les tailles

--- srcs/ft_string.c
+++ srcs/ft_string.c
@@ -23,3 +23,22 @@ char    *ft_strdup(char *str)
     ret [n] = '\0';
     return (ret);
 }
+
+char *ft_strcat(char *src1, char *src2)
+{
+    int n;
+    int i;
+
+    i = 0;
+    n = 0;
+
+    while(src1[n])
+        n ++;
+    while(src2[i])
+    {
+        src1[n] = src2[i];
+        i ++;
+        n ++;
+    }
+    src1[n] = '\0';
+}
